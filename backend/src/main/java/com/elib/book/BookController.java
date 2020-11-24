8557eda3a6b9f79a44e9d195d5f2fe64847bbf91
soroka-ihor <soroka.igor.olegovych@gmail.com>
2020-11-24 16:14:19 +0200

    [+] Added book controller and mapper

---
+++ backend/src/main/java/com/elib/book/BookController.java
@@ -0,0 +1,9 @@
+package com.elib.book;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class BookController {
+}
---
+++ backend/src/main/java/com/elib/book/BookMapper.java
@@ -0,0 +1,24 @@
+package com.elib.book;
+
+import com.elib.book.dto.BookDto;
+import com.elib.book.model.Book;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface BookMapper {
+
+    BookMapper MAPPER = Mappers.getMapper(BookMapper.class);
+
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "genre.name", target = "genre")
+    @Mapping(source = "name", target = "name")
+    @Mapping(source = "author.name", target = "author")
+    @Mapping(source = "publish_year", target = "year")
+    @Mapping(source = "language.name", target = "language")
+    @Mapping(source = "description", target = "description")
+    @Mapping(source = "fileName", target = "contentUrl")
+    BookDto bookToBookDto(Book book);
+
+}
