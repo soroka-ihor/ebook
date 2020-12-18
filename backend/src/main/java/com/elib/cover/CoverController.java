807995a45baac6fcf357b69a747867e5a9bf4ec9
soroka-ihor <soroka.igor.olegovych@gmail.com>
2020-12-18 14:34:59 +0200

    [*] Made GET request for a single cover

--- backend/src/main/java/com/elib/cover/CoverController.java
+++ backend/src/main/java/com/elib/cover/CoverController.java
@@ -3,14 +3,21 @@ package com.elib.cover;
 import com.elib.cover.dto.CoverDto;
 import com.elib.cover.model.Cover;
 import com.elib.storage.files.FilesStorageService;
+
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/cover")
 @CrossOrigin("http://localhost:3000")
 public class CoverController {
 
@@ -33,9 +40,10 @@ public class CoverController {
         }
     }
 
-    @GetMapping("/covers")
-    public ResponseEntity<List<CoverDto>> getListCovers() {
-        return ResponseEntity.status(HttpStatus.OK).body(coverService.getAllCovers());
+    @GetMapping(value = "{id}", produces = MediaType.IMAGE_JPEG_VALUE)
+    public @ResponseBody byte[] getCover(@PathVariable Long id) throws IOException {
+        InputStream is = new FileInputStream(storageService.downloadCover(coverService.getById(id).getFileName()).getFile());
+        return IOUtils.toByteArray(is);
     }
 
 }
